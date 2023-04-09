b9dc2977e5aba7b06adf060c55845d17e0034fc5
Nadia Carolina Santillan <nadia@pop-os.localdomain>
2023-04-09 14:46:35 -0300

    Improving mock

--- libros-ui/src/app/mock-books.ts
+++ libros-ui/src/app/mock-books.ts
@@ -13,8 +13,8 @@ export const BOOKS: Book[] = [
     {
         title: 'El péndulo de Foucault',
         author: 'Humberto Eco',
-        description: 'Valiéndose de características propias de la novela gótica, la crónica medieval, la novela policíaca, el relato ideológico en clave y la alegoría narrativa, El nombre de la rosa narra las actividades detectivescas de Guillermo de Baskerville para esclarecer los crímenes cometidos en una abadía benedictina... Y a esta apacionante trama debe sumarse la admirable reconstrucción de una época especialmente conflictiva, reconstrucción que no se detiene en lo exterior sino que ahonda en las formas de pensar y sentir del siglo VXI.',
-        price: "$ 2500.00",
+        description: 'Tres intelectuales que trabajan en una editorial de Milán establecen contacto con autores interesados en las ciencias ocultas, las sociedades secretas y las conjuras cósmicas. En un primer momento dicha relación se mantiene estrictamente profesional, pero poco a poco van estrechándose los lazos. Editores y autores inventan untos, por puro juego, un complejo «plan», urdido supuestamente por los templarios siete siglos atrás. Pero alguien toma demasiado en serio el juego, y todos ellos se verán inmersos en una inquietante pesadilla.',
+        price: "$ 3500.00",
         deliveryPrice: "$ 300.00",
         imagePath: "elpendulodefoucault_portada.webp",
     },
